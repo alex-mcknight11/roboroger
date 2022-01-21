1723ed77520eb69bcb8d0af12399d54fad2515fc
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-01-21 11:02:12 -0800

    refine JS Code

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,22 @@ let numbers = 'inputField';
 let beepInput = '1';
 let boopInput = '2';
 let neighborInput = '3';
-let residualArray = [];
+let numberArray = [];
 
 var numberSubmission = parseInt($('inputField').val());
 
-if (numbers.includes('1')) {
-	console.log('Beep!');
-}
-
-if (numbers.includes('2')) {
-	console.log('Boop!');
-}
-
-if (numbers.includes('3')) {
-	console.log("Won't you be my neighbor?");
+function checkForMatch(numbers) {
+	for (let i = 0; (inputField) => i; i++) {
+		const element = i.toString();
+		if (numbers.includes('1')) {
+			numberArray.push('Beep!');
+			console.log('Beep!');
+		} else if (numbers.includes('2')) {
+			console.log('Boop!');
+			numberArray.push('Boop!');
+		} else if (numbers.includes('3')) {
+			console.log("Won't you be my neighbor?");
+			numberArray.push("Won't you be my neighbor?");
+		}
+	}
 }
