0b85fbb9c2c374557441896500f7c33684e857fb
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-01-21 15:56:02 -0800

    Add code to help make Array visible

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,38 @@
 // let beepInput = '1';
 // let boopInput = '2';
 // let neighborInput = '3';
-let numberArray = [];
 
-function checkForMatch(number) {
-	console.log(number);
-	const numberArray = [];
-	for (let i = 0; number >= i; i++) {
-		const element = i.toString();
-		if (element.includes('3')) {
-			numberArray.push("Won't you be my neighbor?");
-			console.log("Won't you be my neighbor?");
-		} else if (element.includes('2')) {
-			console.log('Boop!');
-			numberArray.push('Boop!');
-		} else if (element.includes('1')) {
-			console.log('Beep!');
-			numberArray.push('Beep!');
-		} else {
-			numberArray.push(element);
+window.onload = function () {
+	let numberArray = [];
+
+	function checkForMatch(number) {
+		console.log(number);
+		const numberArray = [];
+		for (let i = 0; number >= i; i++) {
+			const element = i.toString();
+			if (element.includes('3')) {
+				numberArray.push("Won't you be my neighbor?");
+				console.log("Won't you be my neighbor?");
+			} else if (element.includes('2')) {
+				console.log('Boop!');
+				numberArray.push('Boop!');
+			} else if (element.includes('1')) {
+				console.log('Beep!');
+				numberArray.push('Beep!');
+			} else {
+				numberArray.push(element);
+			}
 		}
+		return numberArray;
 	}
-	return numberArray;
-}
 
-//UI LOGIC
-$(document).ready(function () {
-	$('form').submit(function (event) {
-		event.preventDefault();
-		let numberSubmission = parseInt($('#inputField').val(), 10);
-		console.log(checkForMatch(numberSubmission));
-		$('#stringOutput').replaceWith(numberArray);
+	//UI LOGIC
+	$(document).ready(function () {
+		$('form').submit(function (event) {
+			event.preventDefault();
+			let numberSubmission = parseInt($('#inputField').val(), 10);
+			console.log(checkForMatch(numberSubmission));
+			$('#stringOutput').replaceWith(numberArray);
+		});
 	});
-});
+};
